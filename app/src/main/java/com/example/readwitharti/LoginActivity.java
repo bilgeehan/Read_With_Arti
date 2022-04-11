0e7fed5f5da0b98c4dce51cb1e0336e023746db2
bilgehan <73483481+bilgeehan@users.noreply.github.com>
2022-04-11 19:27:49 +0300

    login and signup designes

---
+++ app/src/main/java/com/example/readwitharti/LoginActivity.java
@@ -0,0 +1,23 @@
+package com.example.readwitharti;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void onClickSignup(View view) {
+
+    }
+
+    public void onClickForgot(View view) {
+
+    }
+}
---
+++ app/src/main/java/com/example/readwitharti/WelcomeActivity.java
@@ -0,0 +1,26 @@
+package com.example.readwitharti;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+
+    public void onClickLogin(View view) {
+        Intent intent = new Intent(WelcomeActivity.this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    public void onClickSignUp(View view) {
+        Intent intent = new Intent(WelcomeActivity.this, SignUpActivity.class);
+        startActivity(intent);
+    }
+}
