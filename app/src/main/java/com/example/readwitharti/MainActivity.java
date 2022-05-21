83aa9a6d0f2c9c6279e0b6f61493d6185bff3aff
bilgehan <73483481+bilgeehan@users.noreply.github.com>
2022-05-21 18:24:29 +0300

    designs updated

--- app/src/main/java/com/example/readwitharti/MainActivity.java
+++ app/src/main/java/com/example/readwitharti/MainActivity.java
@@ -5,23 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.widget.ImageView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
 
 import java.util.ArrayList;
 
@@ -37,32 +29,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         recyclerView = findViewById(R.id.recyclerview);
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(MainActivity.this);
         recyclerView.setLayoutManager(linearLayoutManager);
         recyclerView.setHasFixedSize(true);
         myRef = FirebaseDatabase.getInstance().getReference();
         stories = new ArrayList<>();
-        clearAll();
-        getDataFromFirebase();
-
+        clearEverything();
+        getDataFromDatabase();
     }
 
-    private void getDataFromFirebase() {
+    private void getDataFromDatabase() {
         Query query = myRef.child("Stories");
         query.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                clearAll();
+                clearEverything();
                 for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
                     Story story = new Story();
                     story.setCoverPhoto(snapshot.child("image").getValue().toString());
                     story.setTitle(snapshot.child("title").getValue().toString());
                     stories.add(story);
                 }
-                System.out.println(stories.get(0).getTitle());
+            /*    System.out.println(stories.get(0).getTitle());
                 System.out.println(stories.get(0).getCoverPhoto());
                 System.out.println(stories.get(1).getTitle());
-                System.out.println(stories.get(1).getCoverPhoto());
+                System.out.println(stories.get(1).getCoverPhoto());*/
                 recyclerAdapter = new RecyclerAdapter(getApplicationContext(), stories);
                 recyclerView.setAdapter(recyclerAdapter);
                 recyclerAdapter.notifyDataSetChanged();
@@ -70,12 +61,12 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-
+                Toast.makeText(MainActivity.this, "!Error! Try again later", Toast.LENGTH_LONG).show();
             }
         });
     }
 
-    private void clearAll() {
+    private void clearEverything() {
         if (stories != null) {
             stories.clear();
             if (recyclerAdapter != null) {
