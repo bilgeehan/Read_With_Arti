d46baeb8d5358edd22378606cb4d82d29c0c0a06
Sezin <218se2122@isik.edu.tr>
2022-05-22 20:01:57 +0300

    Added About Us button in main. Added About Us page again.

--- app/src/main/java/com/example/readwitharti/WelcomeActivity.java
+++ app/src/main/java/com/example/readwitharti/WelcomeActivity.java
@@ -23,4 +23,8 @@ public class WelcomeActivity extends AppCompatActivity {
         Intent intent = new Intent(WelcomeActivity.this, SignUpActivity.class);
         startActivity(intent);
     }
+    public void onClickSAboutUs(View view) {
+        Intent intent = new Intent(WelcomeActivity.this, AboutUs.class);
+        startActivity(intent);
+    }
 }
