1ffa893d874403a5e07e06e3de6ad41e8fa37792
Sezin <218se2122@isik.edu.tr>
2022-05-22 16:11:27 +0300

    Search Adapter is done. Search Story Activity is done. Activity Search Story is done. Search menu is done. Story item is done.

---
+++ app/src/main/java/com/example/readwitharti/SearchAdapter.java
@@ -0,0 +1,53 @@
+package com.example.readwitharti;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+
+import java.util.ArrayList;
+
+public class SearchAdapter extends RecyclerView.Adapter<SearchAdapter.MyViewHolder> {
+Context context;
+ArrayList<Story> listt;
+
+    public SearchAdapter(Context context, ArrayList<Story> listt) {
+        this.context=context;
+        this.listt=listt;
+    }
+
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v=LayoutInflater.from(context).inflate(R.layout.story_item,parent,false);
+
+        return new MyViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+Story story=listt.get(position);
+holder.storyy.setText(story.getStory());
+holder.titlee.setText(story.getTitle());
+    }
+
+    @Override
+    public int getItemCount() {
+        return listt.size();
+    }
+
+    public class MyViewHolder  extends RecyclerView.ViewHolder{
+        TextView storyy,titlee;
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            storyy=itemView.findViewById(R.id.inputStory);
+            titlee=itemView.findViewById(R.id.inputTitleStory);
+        }
+    }
+}
