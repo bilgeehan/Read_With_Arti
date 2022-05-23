bc8cc8a466030056d9848ad9e8aff228c61b4168
bilgehan <73483481+bilgeehan@users.noreply.github.com>
2022-05-24 01:59:45 +0300

    some improvements

--- app/src/main/java/com/example/readwitharti/SpeechToStoryActivity.java
+++ app/src/main/java/com/example/readwitharti/SpeechToStoryActivity.java
@@ -1,5 +1,6 @@
 package com.example.readwitharti;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -12,13 +13,22 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
 import java.util.Locale;
 
 public class SpeechToStoryActivity extends AppCompatActivity {
     private TextView title;
+    ArrayList<String> userTalks;
+    ArrayList<String> arrStory;
     private String strTitle;
     private TextView story;
+    private DatabaseReference myRef;
     private String userTalk;
     private ImageView micButton;
     private SpeechRecognizer speechRecognizer;
@@ -29,15 +39,35 @@ public class SpeechToStoryActivity extends AppCompatActivity {
         setContentView(R.layout.activity_speech_to_story);
         title = findViewById(R.id.textTitle);
         story = findViewById(R.id.textStory);
+        micButton = findViewById(R.id.imageView20);
+        myRef = FirebaseDatabase.getInstance().getReference();
+        userTalk = "";
+        arrStory=new ArrayList<>();
+        userTalks=new ArrayList<>();
+
         Intent intent = getIntent();
         strTitle = intent.getStringExtra("chosenTitle");
         title.setText(strTitle);
-        userTalk = "";
-        micButton = findViewById(R.id.imageView20);
+        getDataFromDatabase();
+
         micButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 speak();
+            }
+        });
+    }
+
+    private void getDataFromDatabase() {
+        myRef.child("Stories").child(strTitle).child("story").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                story.setText(dataSnapshot.getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(SpeechToStoryActivity.this, "!Error! Try again later", Toast.LENGTH_LONG).show();
             }
         });
     }
@@ -87,9 +117,10 @@ public class SpeechToStoryActivity extends AppCompatActivity {
                 for (int i = 0; i < data.size(); i++) {
                     userTalk += data.get(i);
                 }
-                story.setText(userTalk);
+                //  story.setText(userTalk);
                 System.out.println(userTalk);
                 micButton.setImageResource(R.drawable.no_sound);
+
             }
 
             @Override
