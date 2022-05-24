1f979b5190c4ab289720fb1531d210285cffc3d7
Sezin <218se2122@isik.edu.tr>
2022-05-24 15:44:06 +0300

    navbar demo

---
+++ app/src/main/java/com/example/readwitharti/MainActivity2.java
@@ -0,0 +1,63 @@
+package com.example.readwitharti;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.readwitharti.databinding.ActivityMain2Binding;
+
+public class MainActivity2 extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    private ActivityMain2Binding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMain2Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        navView.setOnNavigationItemSelectedListener(MainActivity2.this);
+
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_search, R.id.navigation_profile)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main2);
+        //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+      //  NavigationUI.setupWithNavController(binding.navView, navController);
+        navView.setOnNavigationItemSelectedListener(this);
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.navigation_home:{
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+            }
+            case R.id.navigation_search:{
+                startActivity(new Intent(this, SearchStoryActivity.class));
+                break;
+            }
+            case R.id.navigation_profile:{
+                startActivity(new Intent(this, ProfileActivity.class));
+                break;
+
+            }
+        }
+        return false;
+    }
+}
--- app/src/main/java/com/example/readwitharti/SignUpActivity.java
+++ app/src/main/java/com/example/readwitharti/SignUpActivity.java
@@ -16,14 +16,18 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import android.widget.CheckBox;
+
 
 public class SignUpActivity extends AppCompatActivity {
     private EditText name;
     private EditText date;
     private EditText email;
     private EditText password;
+    private EditText repeatedpassword;
     private FirebaseAuth mAuth;
     private DatabaseReference mDatabase;
+    private CheckBox checkBox;
 
 
     @Override
@@ -36,38 +40,57 @@ public class SignUpActivity extends AppCompatActivity {
         date = (EditText) findViewById(R.id.inputDate);
         email = (EditText) findViewById(R.id.inputMail);
         password = (EditText) findViewById(R.id.inputPassword);
+        repeatedpassword=(EditText) findViewById(R.id.inputRepeatPassword);
+        checkBox = (CheckBox) findViewById(R.id.checkBox);
 
     }
+
 
     public void onClickButton(View view) {
         String stringName = name.getText().toString();
         String stringDate = date.getText().toString();
         String stringEmail = email.getText().toString();
         String stringPassword = password.getText().toString();
+        String stringrepeatPassword= repeatedpassword.getText().toString();
 
-        if (stringName.isEmpty() || stringDate.isEmpty() || stringEmail.isEmpty() || stringPassword.isEmpty()) {
-            Toast.makeText(SignUpActivity.this, "Please fill in the blank spaces", Toast.LENGTH_LONG).show();
+
+        if (stringName.isEmpty() || stringDate.isEmpty() || stringEmail.isEmpty() || stringPassword.isEmpty()|| stringrepeatPassword.isEmpty()) {
+            Toast.makeText(SignUpActivity.this, "Please fill in the blank spaces and if not, Agree to the Terms of Policy", Toast.LENGTH_LONG).show();
         } else {
-            if (stringPassword.length() >= 6) {
-                mAuth.createUserWithEmailAndPassword(stringEmail, stringPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            mDatabase.child("Users").child(mAuth.getUid()).child("name").setValue(stringName);
-                            mDatabase.child("Users").child(mAuth.getUid()).child("date").setValue(stringDate);
-                            mDatabase.child("Users").child(mAuth.getUid()).child("mail").setValue(stringEmail);
+            if(stringPassword.length() >= 6 ){
+            if (stringrepeatPassword.equals(stringPassword)) {
+                if (checkBox.isChecked()) {
 
-                            Toast.makeText(SignUpActivity.this, "User successfully created", Toast.LENGTH_SHORT).show();
-                            Intent registerIntent = new Intent(SignUpActivity.this, LoginActivity.class);
-                            startActivity(registerIntent);
-                        } else {
-                            Toast.makeText(SignUpActivity.this, "User cannot be created. Please try again", Toast.LENGTH_SHORT).show();
+                    mAuth.createUserWithEmailAndPassword(stringEmail, stringPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                mDatabase.child("Users").child(mAuth.getUid()).child("name").setValue(stringName);
+                                mDatabase.child("Users").child(mAuth.getUid()).child("date").setValue(stringDate);
+                                mDatabase.child("Users").child(mAuth.getUid()).child("mail").setValue(stringEmail);
+
+                                Toast.makeText(SignUpActivity.this, "User successfully created", Toast.LENGTH_SHORT).show();
+                                Intent registerIntent = new Intent(SignUpActivity.this, LoginActivity.class);
+                                startActivity(registerIntent);
+                            } else {
+                                Toast.makeText(SignUpActivity.this, "User cannot be created. Please try again", Toast.LENGTH_SHORT).show();
+                            }
                         }
-                    }
-                });
+                    });
+                } else {
+                    Toast.makeText(SignUpActivity.this, "You have to agree to the terms of policy to sign up.", Toast.LENGTH_LONG).show();
+
+                }
             } else {
                 Toast.makeText(SignUpActivity.this, "Please create a password of at least 6 characters", Toast.LENGTH_SHORT).show();
             }
+        } else{
+                Toast.makeText(SignUpActivity.this, "Please enter the same string as password in repeated password blank", Toast.LENGTH_SHORT).show();
+            }
+
+        }
+
+
         }
     }
-}
+
