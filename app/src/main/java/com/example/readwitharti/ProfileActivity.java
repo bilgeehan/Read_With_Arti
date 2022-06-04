8a41e46f408c59b37d364e3a4f24b0d1c671ed04
bilgehan <73483481+bilgeehan@users.noreply.github.com>
2022-06-05 01:11:49 +0300

    Some Improvements

--- app/src/main/java/com/example/readwitharti/ProfileActivity.java
+++ app/src/main/java/com/example/readwitharti/ProfileActivity.java
@@ -9,6 +9,8 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -20,14 +22,21 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Date;
 
-public class ProfileActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
+public class ProfileActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     private RecyclerView recyclerVieww;
     private DatabaseReference mDatabase;
     private FirebaseAuth mAuth;
     private ArrayList<Statistics> statisticsArrayList;
     private StatisticsAdapter statisticsAdapter;
+    private String name;
+    private String date;
+    private TextView textName;
+    private TextView textDate;
+    private final int currentYear = 2022;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +47,8 @@ public class ProfileActivity extends AppCompatActivity implements BottomNavigati
         recyclerVieww = findViewById(R.id.recyclervieww);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         recyclerVieww.setLayoutManager(linearLayoutManager);
+        textName = findViewById(R.id.textView22);
+        textDate = findViewById(R.id.textView24);
         recyclerVieww.setHasFixedSize(true);
         statisticsArrayList = new ArrayList<>();
 
@@ -49,7 +60,43 @@ public class ProfileActivity extends AppCompatActivity implements BottomNavigati
         // NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main2);
         navView.setOnNavigationItemSelectedListener((BottomNavigationView.OnNavigationItemSelectedListener) this);
 
+        takeStatisticsFromDatabase();
+        takeUserInformation();
 
+    }
+
+    private void takeUserInformation() {
+        mDatabase.child("Users").child(mAuth.getUid()).child("name").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                name = dataSnapshot.getValue().toString();
+                textName.setText(name);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(ProfileActivity.this, error.getCode(), Toast.LENGTH_LONG).show();
+            }
+        });
+        mDatabase.child("Users").child(mAuth.getUid()).child("date").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                date = dataSnapshot.getValue().toString();
+                String arr[] = date.split("/");
+                //System.out.println(arr[2]);
+                int yearr = Integer.parseInt(arr[2]);
+                textDate.setText(String.valueOf(currentYear - yearr));
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(ProfileActivity.this, error.getCode(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void takeStatisticsFromDatabase() {
         mDatabase.child("Users").child(mAuth.getUid()).child("Stories").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
@@ -76,21 +123,28 @@ public class ProfileActivity extends AppCompatActivity implements BottomNavigati
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()){
-            case R.id.navigation_home:{
+        switch (item.getItemId()) {
+            case R.id.navigation_home: {
                 startActivity(new Intent(this, MainActivity.class));
                 break;
             }
-            case R.id.navigation_search:{
-                  startActivity(new Intent(this, SearchActivity.class));
+            case R.id.navigation_search: {
+                startActivity(new Intent(this, SearchActivity.class));
                 break;
             }
-            case R.id.navigation_profile:{
+            case R.id.navigation_profile: {
                 //startActivity(new Intent(this, ProfileActivity.class));
                 break;
 
             }
         }
         return false;
+    }
+
+    public void logout(View view) {
+        mAuth.signOut();
+        Intent intent = new Intent(this, WelcomeActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
--- app/src/main/java/com/example/readwitharti/SignUpActivity.java
+++ app/src/main/java/com/example/readwitharti/SignUpActivity.java
@@ -3,9 +3,12 @@ package com.example.readwitharti;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -18,17 +21,22 @@ import com.google.firebase.database.FirebaseDatabase;
 
 import android.widget.CheckBox;
 
+import java.util.Calendar;
+
 
 public class SignUpActivity extends AppCompatActivity {
     private EditText name;
-    private EditText date;
     private EditText email;
     private EditText password;
     private EditText repeatedpassword;
     private FirebaseAuth mAuth;
     private DatabaseReference mDatabase;
     private CheckBox checkBox;
-
+    private Button selectDate;
+    private DatePickerDialog datePickerDialog;
+    private Calendar calendar;
+    private int year, month, day;
+    private String stringDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,60 +45,70 @@ public class SignUpActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         mDatabase = FirebaseDatabase.getInstance().getReference();
         name = (EditText) findViewById(R.id.inputName);
-        date = (EditText) findViewById(R.id.inputDate);
         email = (EditText) findViewById(R.id.inputMail);
         password = (EditText) findViewById(R.id.inputPassword);
-        repeatedpassword=(EditText) findViewById(R.id.inputRepeatPassword);
+        repeatedpassword = (EditText) findViewById(R.id.inputRepeatPassword);
         checkBox = (CheckBox) findViewById(R.id.checkBox);
-
+        selectDate = findViewById(R.id.button10);
+        selectDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calendar = Calendar.getInstance();
+                year = calendar.get(Calendar.YEAR);
+                month = calendar.get(Calendar.MONTH);
+                day = calendar.get(Calendar.DAY_OF_MONTH);
+                datePickerDialog = new DatePickerDialog(SignUpActivity.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                                stringDate = day + "/" + (month + 1) + "/" + year;
+                                selectDate.setText(stringDate);
+                                System.out.println(stringDate);
+                            }
+                        }, year, month, day);
+                datePickerDialog.show();
+            }
+        });
     }
-
 
     public void onClickButton(View view) {
         String stringName = name.getText().toString();
-        String stringDate = date.getText().toString();
         String stringEmail = email.getText().toString();
         String stringPassword = password.getText().toString();
-        String stringrepeatPassword= repeatedpassword.getText().toString();
+        String stringRepeatPassword = repeatedpassword.getText().toString();
 
-
-        if (stringName.isEmpty() || stringDate.isEmpty() || stringEmail.isEmpty() || stringPassword.isEmpty()|| stringrepeatPassword.isEmpty()) {
+        if (stringName.isEmpty() || stringDate.isEmpty() || stringEmail.isEmpty() || stringPassword.isEmpty() || stringRepeatPassword.isEmpty()) {
             Toast.makeText(SignUpActivity.this, "Please fill in the blank spaces and if not, Agree to the Terms of Policy", Toast.LENGTH_LONG).show();
         } else {
-            if(stringPassword.length() >= 6 ){
-            if (stringrepeatPassword.equals(stringPassword)) {
-                if (checkBox.isChecked()) {
+            if (stringPassword.length() >= 6) {
+                if (stringRepeatPassword.equals(stringPassword)) {
+                    if (checkBox.isChecked()) {
+                        mAuth.createUserWithEmailAndPassword(stringEmail, stringPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    mDatabase.child("Users").child(mAuth.getUid()).child("name").setValue(stringName);
+                                    mDatabase.child("Users").child(mAuth.getUid()).child("date").setValue(stringDate);
+                                    mDatabase.child("Users").child(mAuth.getUid()).child("mail").setValue(stringEmail);
 
-                    mAuth.createUserWithEmailAndPassword(stringEmail, stringPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful()) {
-                                mDatabase.child("Users").child(mAuth.getUid()).child("name").setValue(stringName);
-                                mDatabase.child("Users").child(mAuth.getUid()).child("date").setValue(stringDate);
-                                mDatabase.child("Users").child(mAuth.getUid()).child("mail").setValue(stringEmail);
-
-                                Toast.makeText(SignUpActivity.this, "User successfully created", Toast.LENGTH_SHORT).show();
-                                Intent registerIntent = new Intent(SignUpActivity.this, LoginActivity.class);
-                                startActivity(registerIntent);
-                            } else {
-                                Toast.makeText(SignUpActivity.this, "User cannot be created. Please try again", Toast.LENGTH_SHORT).show();
+                                    Toast.makeText(SignUpActivity.this, "User successfully created", Toast.LENGTH_SHORT).show();
+                                    Intent registerIntent = new Intent(SignUpActivity.this, LoginActivity.class);
+                                    startActivity(registerIntent);
+                                } else {
+                                    Toast.makeText(SignUpActivity.this, "User cannot be created. Please try again", Toast.LENGTH_SHORT).show();
+                                }
                             }
-                        }
-                    });
+                        });
+                    } else {
+                        Toast.makeText(SignUpActivity.this, "You have to agree to the terms of policy to sign up.", Toast.LENGTH_LONG).show();
+                    }
                 } else {
-                    Toast.makeText(SignUpActivity.this, "You have to agree to the terms of policy to sign up.", Toast.LENGTH_LONG).show();
-
+                    Toast.makeText(SignUpActivity.this, "Please create a password of at least 6 characters", Toast.LENGTH_SHORT).show();
                 }
             } else {
-                Toast.makeText(SignUpActivity.this, "Please create a password of at least 6 characters", Toast.LENGTH_SHORT).show();
-            }
-        } else{
                 Toast.makeText(SignUpActivity.this, "Please enter the same string as password in repeated password blank", Toast.LENGTH_SHORT).show();
             }
-
-        }
-
-
         }
     }
+}
 
